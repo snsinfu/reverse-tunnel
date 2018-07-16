a132669635d04b59964519d80f772bc88f214017
snsinfu <snsinfu@gmail.com>
2018-07-17 04:35:12 +0900

    Add default agent configuration

--- config/agent.go
+++ config/agent.go
@@ -4,6 +4,11 @@ import (
 	"github.com/snsinfu/reverse-tunnel/ports"
 )
 
+// AgentDefault is a default agent configuration.
+var AgentDefault = Agent{
+	GatewayURL: "ws://localhost:9000",
+}
+
 // Agent represnets a configuration of a reverse tunnel agent program.
 type Agent struct {
 	GatewayURL string    `yaml:"gateway_url"`
