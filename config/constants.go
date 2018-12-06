432f05a9414814990c27787bff599a427667210e
snsinfu <snsinfu@gmail.com>
2018-12-06 20:14:58 +0900

    Change BufferSize to a larger value
    Hopefully this fixes the UDP corruption issue:
    
    https://github.com/snsinfu/reverse-tunnel/issues/3

--- config/constants.go
+++ config/constants.go
@@ -1,5 +1,5 @@
 package config
 
 // BufferSize is the max size of a single message transferred via a tunnel. This
-// should be smaller than MTU minus overhead to prevent fragmentation.
-const BufferSize = 1400
+// should be sufficiently large to prevent fragmentation.
+const BufferSize = 1500
