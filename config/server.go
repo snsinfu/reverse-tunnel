55fd1d3a904d8eb3dddaf407184eaef62140274a
snsinfu <snsinfu@gmail.com>
2019-05-05 00:09:21 +0900

    Add Let's Encrypt support

--- config/server.go
+++ config/server.go
@@ -7,12 +7,20 @@ import (
 // ServerDefault is a default server configuration.
 var ServerDefault = Server{
 	ControlAddress: "localhost:9000",
+	LetsEncrypt:    LetsEncrypt{CacheDir: ".autocert_cache"},
 }
 
 // Server represents a configuration of a reverse tunnel server program.
 type Server struct {
 	ControlAddress string      `yaml:"control_address"`
+	LetsEncrypt    LetsEncrypt `yaml:"lets_encrypt"`
 	Agents         []AgentAuth `yaml:"agents"`
+}
+
+// LetsEncrypt represents autocert configuration for the server.
+type LetsEncrypt struct {
+	Domain   string `yaml:"domain"`
+	CacheDir string `yaml:"cache_dir"`
 }
 
 // AgentAuth represents an agent and its access rights authorized in a reverse
--- server/server.go
+++ server/server.go
@@ -4,11 +4,22 @@ import (
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"github.com/snsinfu/reverse-tunnel/config"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 // Start starts tunneling server with given configuration.
 func Start(conf config.Server) error {
 	e := echo.New()
+
+	// Enable TLS when Let's Encrypt domain is configured. Do not require the
+	// control address port to be 443 because the port could be redirected.
+	useTLS := (conf.LetsEncrypt.Domain != "")
+
+	if useTLS {
+		e.AutoTLSManager.Prompt = autocert.AcceptTOS
+		e.AutoTLSManager.HostPolicy = autocert.HostWhitelist(conf.LetsEncrypt.Domain)
+		e.AutoTLSManager.Cache = autocert.DirCache(conf.LetsEncrypt.CacheDir)
+	}
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
@@ -18,5 +29,8 @@ func Start(conf config.Server) error {
 	e.GET("/udp/:port", action.GetUDPPort)
 	e.GET("/session/:id", action.GetSession)
 
+	if useTLS {
+		return e.StartAutoTLS(conf.ControlAddress)
+	}
 	return e.Start(conf.ControlAddress)
 }
